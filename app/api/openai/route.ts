b580eb3e9d496ccb4429a97b0986c0c4c772712a
sunls24 <sunls24@outlook.com>
2023-11-08 11:11:41 +0800

    Use model gpt-3.5-turbo-1106

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
 
     openai.apiKey = await apiKeyPool.getNextEdge();
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-16k",
+      model: "gpt-3.5-turbo-1106",
       stream: true,
       temperature: 0.5,
       messages: [
